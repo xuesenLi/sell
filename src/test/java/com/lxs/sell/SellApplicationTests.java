886264e12a031589794d870ad8acc1606b23790c
xuesenLi <=>
2020-01-06 10:25:12 +0800

    3 commit

---
+++ src/test/java/com/lxs/sell/SellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxs.sell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
